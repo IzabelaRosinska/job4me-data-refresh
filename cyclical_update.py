2eea3e393d82992ed6d0628c440523bf0d3ebb78
Wiktor Jeżowski <260426@student.pwr.edu.pl>
2023-12-08 14:15:42 +0100

    MIWM-402: Fix bugs and add job_fairs generating

--- cyclical_update.py
+++ cyclical_update.py
@@ -10,7 +10,7 @@ from process_data import *
 from utils.utils import get_json_writable_embeddings
 
 server = os.getenv('AZURE_DB')
-database = 'miwm'
+database = 'job4me'
 db_username = os.getenv('AZURE_DB_USER')
 db_password = os.getenv('AZURE_DB_PASSWORD')
 driver = '{ODBC Driver 17 for SQL Server}'
@@ -20,10 +20,15 @@ UPDATE_OFFERS = True
 SAVE_CHANGES_TO_FILES = False
 
 url = 'https://job4me-recommendation.azurewebsites.net'
+# url = 'http://127.0.0.1:5000'
 
 headers = {
-    'X-API-Key': os.getenv('API_KEY')
+    "Content-Type": "application/json",
+    'API-Key': os.getenv('API_KEY')
 }
+
+print(requests.get(url + '/recommend/1/1', headers=headers).text)
+
 
 transformer = SentenceTransformer('sentence-transformers/LaBSE')
 
@@ -34,7 +39,7 @@ def update_embeddings():
     cursor = conn.cursor()
 
     if UPDATE_EMPLOYEES:
-        employees_to_update = get_employers_to_update(cursor)
+        employees_to_update = get_employees_to_update(cursor)
         print(f'{len(employees_to_update)} employees to update')
         employees_embeddings = get_employees_embeddings(employees_to_update, transformer)
         for employee_id, embeddings in employees_embeddings.items():
@@ -42,10 +47,11 @@ def update_embeddings():
             conn.commit()
         if SAVE_CHANGES_TO_FILES:
             update_json('files/employees_embeddings.json', get_json_writable_embeddings(employees_embeddings))
-        requests.get(url + '/update_employees_embeddings', headers)
+        if employees_to_update:
+            print(requests.get(url + '/update-employees-embeddings', headers).text)
 
     if UPDATE_OFFERS:
-        offers_to_update = get_filtered_offers(cursor, 'actual')
+        offers_to_update = get_filtered_offers(cursor, 'c')
         print(f'{len(offers_to_update)} offers_to update')
         offers_embeddings = get_offers_embeddings(offers_to_update, transformer)
         for offer_id, embeddings in offers_embeddings.items():
@@ -53,7 +59,8 @@ def update_embeddings():
             conn.commit()
         if SAVE_CHANGES_TO_FILES:
             update_json('files/offers_embeddings.json', offers_embeddings)
-        requests.get(url + '/update_offers_embeddings', headers)
+        if offers_to_update:
+            print(requests.get(url + '/update-offers-embeddings', headers).text)
 
     conn.commit()
     conn.close()
